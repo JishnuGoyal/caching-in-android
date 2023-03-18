3a3a814d9968e040049432b7c8edf5ecbe015d6a
JishnuGoyal <64526117+JishnuGoyal@users.noreply.github.com>
2023-03-18 21:22:17 +0530

    setup di

--- app/src/main/java/com/example/cachinginandroid/di/AppModule.kt
+++ app/src/main/java/com/example/cachinginandroid/di/AppModule.kt
@@ -1,9 +1,15 @@
 package com.example.cachinginandroid.di
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.util.LruCache
+import coil.ImageLoader
+import coil.request.CachePolicy
 import com.example.cachinginandroid.api.DogsApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
